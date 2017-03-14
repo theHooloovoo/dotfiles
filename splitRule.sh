f1be4c16dcd7a83e753aed742039a4b9c3fc5a64
Eric <theHooloovoo@protonmail.com>
2017-03-14 11:52:04 -0400

    Added seperate file to reference glyphs. (For human use)

--- splitRule.sh
+++ splitRule.sh
@@ -4,8 +4,8 @@ split=$(xwininfo -id $(bspc query -N -n focused) | awk '/(Width)|(Height)/ {prin
 echo $split | {
 	read width height
 	if [ $width -gt $height ] ; then
-		echo "split_dir=east" "split_ratio=0.7"
+		echo 'split_dir=east' 'split_ratio=0.7'
 	else
-		echo "split_dir=south" "split_ratio=0.7"
+		echo 'split_dir=south' 'split_ratio=0.7'
 	fi
 }
